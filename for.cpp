da4c7892c12fedf673420cb98a9f71369df5ac6b
NhiTran2022 <nhi.tranle2022@gmail.com>
2023-01-12 19:48:29 -0600

    update for loop

--- for.cpp
+++ for.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 int main()
 {
-    for (int i; i <= 10; i++)
+    //print Hello there 10 times
+    for (int i=1/*initialization*/; i <= 10/*condition*/; i++/*counter updation*/)
     {
-        std::cout << "Hello there" << std::endl;
+        std::cout << "Hello there" << std::endl; /*statement*/
     }
+    
+    // Sum from 1 to 100
+    int sum = 0;
+    for (int i = 1; i <= 100; i++)
+    {
+        sum += i;
+    }
+    std::cout << "Sum of 1 to 100 is: " << sum << std::endl;
+    
+
+    // Sum of 1 to an input value
+    int total = 0, val;
+    std::cout << "Enter the number till which the sum has to be calculated: ";
+    std::cin >> val;
+    for (int i = 1; i <= val; i++)
+    {
+        total += i;
+    }
+    std::cout << "Sum of 1 to " << val << " = " << total << std::endl;
     return 0;
 }
